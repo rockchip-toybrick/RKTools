2090dba8f12f715d2454dd6cddb478b0e146e221
Hans Yang <yhx@rock-chips.com>
2015-12-30 16:55:01 +0800

    AndroidTool: update for rk312x marshmallow
    Change-Id: Ic9a0aa7f0c1c1e7026f2c91fc1fd0025a42230fd
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- windows/AndroidTool/rockdev/mkupdate.bat
+++ windows/AndroidTool/rockdev/mkupdate.bat
@@ -2,7 +2,7 @@ Afptool -pack .\backupimage backupimage\backup.img
 Afptool -pack ./ Image\update.img
 
 
-RKImageMaker.exe -RK312A rk3126MiniLoaderAll(L)_V2.20.bin  Image\update.img update.img -os_type:androidos
+RKImageMaker.exe -RK312A RK3126MiniLoaderAll_V2.25.bin  Image\update.img update.img -os_type:androidos
 
 rem update.img is new format, Image\update.img is old format, so delete older format
 del  Image\update.img
