079eadf02509b5ccf29300f396f73ed2913a1d9a
Hans Yang <yhx@rock-chips.com>
2016-01-08 14:12:24 +0800

    RKTools: init for RK3368 Marshmallow
    Change-Id: I59d0b2bf46076bb45ab048e017a9e44a9b8d6a4c
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- windows/AndroidTool/rockdev/mkupdate.bat
+++ windows/AndroidTool/rockdev/mkupdate.bat
@@ -2,7 +2,7 @@ Afptool -pack .\backupimage backupimage\backup.img
 Afptool -pack ./ Image\update.img
 
 
-RKImageMaker.exe -RK312A RK3126MiniLoaderAll_V2.25.bin  Image\update.img update.img -os_type:androidos
+RKImageMaker.exe -RK330A RK3368MiniLoaderAll_V2.50.bin  Image\update.img update.img -os_type:androidos
 
 rem update.img is new format, Image\update.img is old format, so delete older format
 del  Image\update.img
