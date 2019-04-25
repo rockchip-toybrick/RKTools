83354ae795cbf61063da65ba39852c5b20c09dec
Xiao Ya peng <yp.xiao@rock-chips.com>
2019-04-25 14:48:19 +0800

    linux:Linux_Pack_Firmware : change RK330A to RK322H for mkupdate.sh
    Signed-off-by: Xiao Ya peng <yp.xiao@rock-chips.com>

--- linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
+++ linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
@@ -15,7 +15,7 @@ if [ ! -f "package-file" ]; then
 #	pause
 fi
 ./afptool -pack ./ Image/update.img || pause
-./rkImageMaker -RK330A Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
+./rkImageMaker -RK322H Image/MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
 echo "Making update.img OK."
 #echo "Press any key to quit:"
 #read -n1 -s key
